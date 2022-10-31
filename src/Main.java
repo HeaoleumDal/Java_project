647e79c1efbe01083dad64bffadf816522dc799e
HeaoleumDal <88084889+HeaoleumDal@users.noreply.github.com>
2022-10-31 11:31:22 +0900

    test board drawing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-----------------------------------------");
+        System.out.println("| ♜  | ♞  | ♝  | ♛  | ♚  | ♝  | ♞  | ♜  |");
+        System.out.println("-----------------------------------------");
+        System.out.println("| ♟  | ♟  | ♟  | ♟  | ♟  | ♟  | ♟  | ♟  |");
+        System.out.println("-----------------------------------------");
+        System.out.println("|    |    |    |    |    |    |    |    |");
+        System.out.println("-----------------------------------------");
+        System.out.println("|    |    |    |    |    |    |    |    |");
+        System.out.println("-----------------------------------------");
+        System.out.println("|    |    |    |    |    |    |    |    |");
+        System.out.println("-----------------------------------------");
+        System.out.println("|    |    |    |    |    |    |    |    |");
+        System.out.println("-----------------------------------------");
+        System.out.println("| ♙  | ♙  | ♙  | ♙  | ♙  | ♙  | ♙  | ♙  |");
+        System.out.println("-----------------------------------------");
+        System.out.println("| ♖  | ♘  | ♗  | ♕  | ♔  | ♗  | ♘  | ♖  |");
+        System.out.println("-----------------------------------------");
+
     }
 }
