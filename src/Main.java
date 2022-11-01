908af523f6efddd8452d6c42acc19d02b40f56f1
HeaoleumDal <88084889+HeaoleumDal@users.noreply.github.com>
2022-11-01 11:07:59 +0900

    board drawing guide
    체스 판 드로잉 예시 제작

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+import javax.swing.plaf.synth.SynthSliderUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-----------------------------------------");
-        System.out.println("| ♜  | ♞  | ♝  | ♛  | ♚  | ♝  | ♞  | ♜  |");
-        System.out.println("-----------------------------------------");
-        System.out.println("| ♟  | ♟  | ♟  | ♟  | ♟  | ♟  | ♟  | ♟  |");
-        System.out.println("-----------------------------------------");
-        System.out.println("|    |    |    |    |    |    |    |    |");
-        System.out.println("-----------------------------------------");
-        System.out.println("|    |    |    |    |    |    |    |    |");
-        System.out.println("-----------------------------------------");
-        System.out.println("|    |    |    |    |    |    |    |    |");
-        System.out.println("-----------------------------------------");
-        System.out.println("|    |    |    |    |    |    |    |    |");
-        System.out.println("-----------------------------------------");
-        System.out.println("| ♙  | ♙  | ♙  | ♙  | ♙  | ♙  | ♙  | ♙  |");
-        System.out.println("-----------------------------------------");
-        System.out.println("| ♖  | ♘  | ♗  | ♕  | ♔  | ♗  | ♘  | ♖  |");
-        System.out.println("-----------------------------------------");
+        System.out.println("=============================================");
+        System.out.println("                     CHESS               ");
+        System.out.println("   ┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓");
+        System.out.println("H  ┃ ♜  ┃ ♞  ┃ ♝  ┃ ♛  ┃ ♚  ┃ ♝  ┃ ♞  ┃ ♜  ┃");
+        System.out.println("   ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
+        System.out.println("G  ┃ ♟  ┃ ♟  ┃ ♟  ┃ ♟  ┃ ♟  ┃ ♟  ┃ ♟  ┃ ♟  ┃");
+        System.out.println("   ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
+        System.out.println("F  ┃    ┃    ┃    ┃    ┃    ┃    ┃    ┃    ┃");
+        System.out.println("   ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
+        System.out.println("E  ┃    ┃    ┃    ┃    ┃    ┃    ┃    ┃    ┃");
+        System.out.println("   ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
+        System.out.println("D  ┃    ┃    ┃    ┃    ┃    ┃    ┃    ┃    ┃");
+        System.out.println("   ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
+        System.out.println("C  ┃ ♙  ┃    ┃    ┃    ┃    ┃    ┃    ┃    ┃");
+        System.out.println("   ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
+        System.out.println("B  ┃ ♙  ┃ ♙  ┃ ♙  ┃ ♙  ┃ ♙  ┃ ♙  ┃ ♙  ┃ ♙  ┃");
+        System.out.println("   ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
+        System.out.println("A  ┃ ♖  ┃ ♘  ┃ ♗  ┃ ♕  ┃ ♔  ┃ ♗  ┃ ♘  ┃ ♖  ┃");
+        System.out.println("   ┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛");
+        System.out.println("      1    2    3    4    5    6    7    8");
+        System.out.println("=============================================");
+        
 
     }
 }
