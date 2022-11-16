c63d138bd4f512a0c81ea9928183998235da5419
Wongoon <rmsgh64@gmail.com>
2022-11-16 15:05:10 +0900

    static update

--- src/game/UI/chess.java
+++ src/game/UI/chess.java
@@ -1,27 +1,27 @@
 package game.UI;
 
 public class chess {
-    public final String nullPiece = "    ";
+    public static final String nullPiece = "    ";
 
-    public final String WhitePawn = " ♙  ";
-    public final String WhiteRook = " ♖  ";
-    public final String WhiteKnight = " ♘  ";
-    public final String WhiteBishop = " ♗  ";
-    public final String WhiteQueen = " ♕  ";
-    public final String WhiteKing = " ♔  ";
+    public static final String WhitePawn = " ♙  ";
+    public static final String WhiteRook = " ♖  ";
+    public static final String WhiteKnight = " ♘  ";
+    public static final String WhiteBishop = " ♗  ";
+    public static final String WhiteQueen = " ♕  ";
+    public static final String WhiteKing = " ♔  ";
 
-    public final String BlackPawn = " ♟  ";
-    public final String BlackRook = " ♜　";
-    public final String BlackKnight = " ♞  ";
-    public final String BlackBishop = " ♝  ";
-    public final String BlackQueen = " ♛  ";
-    public final String BlackKing = " ♚  ";
+    public static final String BlackPawn = " ♟  ";
+    public static final String BlackRook = " ♜　";
+    public static final String BlackKnight = " ♞  ";
+    public static final String BlackBishop = " ♝  ";
+    public static final String BlackQueen = " ♛  ";
+    public static final String BlackKing = " ♚  ";
 
-    public final String[] WhitePiece = {WhitePawn, WhiteRook, WhiteKnight, WhiteBishop, WhiteQueen, WhiteKing};
-    public final String[] BlackPiece = {BlackPawn, BlackRook, BlackKnight, BlackBishop, BlackQueen, BlackKing};
-    public final String[] Piece = {"Pawn", "Rook", "Knight", "Bishop", "Queen", "King"};
+    public static final String[] WhitePiece = {WhitePawn, WhiteRook, WhiteKnight, WhiteBishop, WhiteQueen, WhiteKing};
+    public static final String[] BlackPiece = {BlackPawn, BlackRook, BlackKnight, BlackBishop, BlackQueen, BlackKing};
+    public static final String[] Piece = {"Pawn", "Rook", "Knight", "Bishop", "Queen", "King"};
 
-    public String[][] chessboard =
+    public static String[][] chessboard =
     {
             { BlackRook, BlackKnight, BlackBishop, BlackQueen, BlackKing, BlackBishop, BlackKnight, BlackRook },
             { BlackPawn, BlackPawn, BlackPawn, BlackPawn, BlackPawn, BlackPawn, BlackPawn, BlackPawn, BlackPawn },
@@ -38,7 +38,7 @@ public class chess {
     }
 
     
-    public void Swap(int now_x, int now_y, int next_x, int next_y, String Piece){
+    public static void Swap(int now_x, int now_y, int next_x, int next_y, String Piece){
         chessboard[next_y][next_x] = Piece;
         chessboard[now_y][now_x] = nullPiece;
     }
