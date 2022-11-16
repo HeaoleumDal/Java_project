70a33fd741e11f10d3da2d352b1db42a2ad179f2
Wongoon <rmsgh64@gmail.com>
2022-11-16 15:05:02 +0900

    pawn update

--- src/game/Piece/PawnPiece.java
+++ src/game/Piece/PawnPiece.java
@@ -2,26 +2,35 @@ package game.Piece;
 import game.UI.chess;
 import game.location.Location;
 public class PawnPiece{
-    chess chess = new chess();
     Location location = new Location();
 
-    public void WhitePawnMove(int now_x, int now_y, int next_x, int next_y) {
-        if(now_y == 6){
-            if(now_y - next_y < 3 && now_y - next_y > 0 && location.NextLocationIsNull(next_x, next_y)){
-                chess.Swap(now_x, now_y, next_x, next_y, chess.WhitePawn);
+    public boolean WhitePawnMove(int now_x, int now_y, int next_x, int next_y) {
+        Boolean NextIsNull = true;
+        for(int i = now_y - 1; i >= next_y; i--){
+            System.out.println(chess.chessboard[now_x][i] + " " + now_x + " " + i);
+            if(location.NextLocationIsNull(next_x, i)){
+                NextIsNull = true;
             }
+            else{
+                NextIsNull = false;
+                break;
+            }
+        }
+        if(NextIsNull){
+            if(now_y == 6){
+                if(now_y - next_y < 3 && now_y - next_y > 0 && location.NextLocationIsNull(next_x, next_y)){
+                    chess.Swap(now_x, now_y, next_x, next_y, chess.WhitePawn);
+                }
+            }
+            else{
+                if(now_y - next_y < 2 && now_y - next_y > 0 && location.NextLocationIsNull(next_x, next_y)){
+                    chess.Swap(now_x, now_y, next_x, next_y, chess.WhitePawn);
+                }
+            }
+            return false;
         }
         else{
-            if(now_y - next_y < 2 && now_y - next_y > 0 && location.NextLocationIsNull(next_x, next_y)){
-                chess.Swap(now_x, now_y, next_x, next_y, chess.WhitePawn);
-            }
-        }
-        
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                System.out.print(chess.chessboard[i][j] + " ");
-            }
-            System.out.println();
+            return true;
         }
     }
 
