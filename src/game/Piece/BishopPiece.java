1e976f37c59a5c6fc9f11fa0f8160900b818e4ef
Wongoon <rmsgh64@gmail.com>
2022-11-22 20:08:01 +0900

    bishop update

--- src/game/Piece/BishopPiece.java
+++ src/game/Piece/BishopPiece.java
@@ -7,186 +7,101 @@ public class BishopPiece{
     public boolean WhiteBishopMove(int now_x, int now_y, int next_x, int next_y) 
     {
         String Piece = location.LocationPiece(now_x, now_y);
-        Boolean canMove = true;
         int xDiff = next_x - now_x;
         int yDiff = next_y - now_y;
-        int xyDiff = xDiff + yDiff;
-
-        //말 잡았을 때
-        if(BishopCatch(now_x, now_y, next_x, next_y)){
-            chess.Swap(now_x, now_y, next_x, next_y, Piece);
-            return true;
-        }
-        
-        //움직일 수 있는 조건
-        if(Math.abs(xDiff) - Math.abs(yDiff) == 0)
-        {
-            canMove = true;
-        }
-        else
-        {
-            canMove = false;
-        }
-
-        //행마하기
-        if(canMove & xyDiff == 0) 
-        {
-            if(xDiff > yDiff) 
-            {
-                for(int i = 1; i < xDiff; i++)
-                {
-                    //x + i, y - i
-                    if(location.LocationIsNull(now_x + i, now_y - i) || !(location.LocationColor(now_x + i, now_y - i) == "White")) 
-                    {
-                        chess.Swap(now_x, now_y, next_x, next_y, Piece);
-                    }
-                }
-            } 
-            else 
-            {
-                for(int i = 1; i < yDiff; i++) 
-                {
-                    //x - i, y + i
-                    if(location.LocationIsNull(now_x - i, now_y + i) || !(location.LocationColor(now_x - i, now_y + i) == "White")) 
-                    {
-                        chess.Swap(now_x, now_y, next_x, next_y, Piece);
+        boolean canMove = Math.abs(xDiff) - Math.abs(yDiff) == 0 ? true : false;
+        Boolean NextIsNull = true;
+        if(canMove){
+            // 오른쪽 위
+            if(now_x < next_x && now_y > next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x + i, now_y - i) == "White"){
+                        NextIsNull = false;
                     }
                 }
             }
-            return false;
-        } 
-
-        else if(canMove & xyDiff > 0) 
-        {
-            for(int i = 1; i < xDiff; i++) 
-            {
-                //x + i, y + i
-                if(location.LocationIsNull(now_x + i, now_y + i) || !(location.LocationColor(now_x + i, now_y + i) == "White")) 
-                {
-                    chess.Swap(now_x, now_y, next_x, next_y, Piece);
+            // 왼쪽 위
+            if(now_x > next_x && now_y > next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x - i, now_y - i) == "White"){
+                        NextIsNull = false;
+                    }
                 }
             }
-            return false;
-        }
-
-        else if(canMove)
-        {
-            for(int i = 1; i < Math.abs(xDiff); i++) 
-            {
-                //x - i, y - i
-                if(location.LocationIsNull(now_x - i, now_y - i) || !(location.LocationColor(now_x - i, now_y - i) == "White")) 
-                {
-                    chess.Swap(now_x, now_y, next_x, next_y, Piece);
+            // 오른쪽 아래
+            if(now_x < next_x && now_y < next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x + i, now_y + i) == "White"){
+                        NextIsNull = false;
+                    }
                 }
             }
-            return false;
+            // 왼쪽 아래
+            if(now_x > next_x && now_y < next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x - i, now_y + i) == "White"){
+                        NextIsNull = false;
+                    }
+                }
+            }
+
+            if(NextIsNull){
+                game.UI.chess.Swap(now_x, now_y, next_x, next_y, Piece);
+                return true;
+            }
+            else{
+                return false;
+            }
         }
-        else
-        {
-            return true;
+        else{
+            return false;
         }
     }
 
-    public Boolean BlackBishopMove(int now_x, int now_y, int next_x, int next_y){
+    public Boolean BlackBishopMove(int now_x, int now_y, int next_x, int next_y)
+    {
         String Piece = location.LocationPiece(now_x, now_y);
-        Boolean canMove = true;
         int xDiff = next_x - now_x;
         int yDiff = next_y - now_y;
-        int xyDiff = xDiff + yDiff;
-
-        //말 잡았을 때
-        if(BishopCatch(now_x, now_y, next_x, next_y)){
-            chess.Swap(now_x, now_y, next_x, next_y, Piece);
-            return true;
-        }
-        
-        //움직일 수 있는 조건
-        if(Math.abs(xDiff) - Math.abs(yDiff) == 0)
-        {
-            canMove = true;
-        }
-        else
-        {
-            canMove = false;
-        }
-
-        //행마하기
-        if(canMove & xyDiff == 0) 
-        {
-            if(xDiff > yDiff) 
-            {
-                for(int i = 1; i < xDiff; i++) 
-                {
-                    //x + i, y - i
-                    if(location.LocationIsNull(now_x + i, now_y - i) || !(location.LocationColor(now_x + i, now_y - i) == "Black")) 
-                    {
-                        chess.Swap(now_x, now_y, next_x, next_y, Piece);
-                    }
-                }
-            } 
-            else 
-            {
-                for(int i = 1;  i < yDiff; i++) 
-                {
-                    //x - i, y + i
-                    if(location.LocationIsNull(now_x - i, now_y + i) || !(location.LocationColor(now_x - i, now_y + i) == "Black")) 
-                    {
-                        chess.Swap(now_x, now_y, next_x, next_y, Piece);
+        boolean canMove = Math.abs(xDiff) - Math.abs(yDiff) == 0 ? true : false;
+        Boolean NextIsNull = true;
+        if(canMove){
+            // 오른쪽 위
+            if(now_x < next_x && now_y > next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x + i, now_y - i) == "Black"){
+                        NextIsNull = false;
                     }
                 }
             }
-            return false;
-        } 
-
-        else if(canMove & xyDiff > 0) 
-        {
-            for(int i = 1; i < xDiff; i++) 
-            {
-                //x + i, y + i
-                if(location.LocationIsNull(now_x + i, now_y + i) || !(location.LocationColor(now_x + i, now_y + i) == "Black")) 
-                {
-                    chess.Swap(now_x, now_y, next_x, next_y, Piece);
+            // 왼쪽 위
+            if(now_x > next_x && now_y > next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x - i, now_y - i) == "Black"){
+                        NextIsNull = false;
+                    }
                 }
             }
-            return false;
-        }
-
-        else if(canMove)
-        {
-            for(int i = 1; i < Math.abs(xDiff); i++) 
-            {
-                //x - i, y - i
-                if(location.LocationIsNull(now_x - i, now_y - i) || !(location.LocationColor(now_x - i, now_y - i) == "Black")) 
-                {
-                    chess.Swap(now_x, now_y, next_x, next_y, Piece);
+            // 오른쪽 아래
+            if(now_x < next_x && now_y < next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x + i, now_y + i) == "Black"){
+                        NextIsNull = false;
+                    }
                 }
             }
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-
-    public Boolean BishopCatch(int now_x, int now_y, int next_x, int next_y){
-        String Color = location.LocationColor(now_x, now_y);
-        if(Math.abs(now_x - next_x) == 1 && Math.abs(now_y - next_y) == 1){
-            if(Color == "White"){
-                if(now_y > next_y){
-                    return true;
-                }
-                else{
-                    return false;
+            // 왼쪽 아래
+            if(now_x > next_x && now_y < next_y){
+                for(int i = 1; i <= Math.abs(xDiff); i++){
+                    if(location.LocationColor(now_x - i, now_y + i) == "Black"){
+                        NextIsNull = false;
+                    }
                 }
             }
-            else if(Color == "Black"){
-                if(now_y < next_y){
-                    return true;
-                }
-                else{
-                    return false;
-                }
+
+            if(NextIsNull){
+                game.UI.chess.Swap(now_x, now_y, next_x, next_y, Piece);
+                return true;
             }
             else{
                 return false;
--- src/game/Piece/RookPiece.java
+++ src/game/Piece/RookPiece.java
@@ -9,7 +9,6 @@ public class RookPiece
     public boolean WhiteRookMove(int now_x, int now_y, int next_x, int next_y) 
     {
         String Piece = location.LocationPiece(now_x, now_y);
-        String Color = location.LocationColor(now_x, now_y);
         Boolean NextIsNull = true;
         int xDiff = now_x - next_x;
         int yDiff = now_y - next_y;
@@ -20,7 +19,7 @@ public class RookPiece
             // 위로
             if(now_y > next_y){
                 for(int i = 1; i <= now_y - next_y; i++){
-                    if(location.LocationColor(now_x, next_y + i) == Color){
+                    if(location.LocationColor(now_x, next_y + i) == "White"){
                         NextIsNull = false;
                     }
                 }
@@ -28,7 +27,7 @@ public class RookPiece
             // 아래로
             else if(now_y < next_y){
                 for(int i = 1; i <= next_y - now_y; i++){
-                    if(location.LocationColor(now_x, now_y + i) == Color){
+                    if(location.LocationColor(now_x, now_y + i) == "White"){
                         NextIsNull = false;
                     }
                 }
@@ -42,11 +41,11 @@ public class RookPiece
                 return false;
             }
         }
-        else if(yDiff == 0){
+        else if(xDiff != 0 && yDiff == 0){
             // 왼쪽으로
             if(now_x > next_x){
                 for(int i = 1; i <= now_x - next_x; i++){
-                    if(location.LocationColor(next_x + i, now_y) == Color){
+                    if(location.LocationColor(next_x + i, now_y) == "White"){
                         NextIsNull = false;
                     }
                 }
@@ -54,7 +53,7 @@ public class RookPiece
             // 오른쪽으로
             else if(now_x < next_x){
                 for(int i = 1; i <= next_x - now_x; i++){
-                    if(location.LocationColor(now_x + i, now_y) == Color){
+                    if(location.LocationColor(now_x + i, now_y) == "White"){
                         NextIsNull = false;
                     }
                 }
@@ -76,7 +75,6 @@ public class RookPiece
     public Boolean BlackRookMove(int now_x, int now_y, int next_x, int next_y)
     {
         String Piece = location.LocationPiece(now_x, now_y);
-        String Color = location.LocationColor(now_x, now_y);
         Boolean NextIsNull = true;
         int xDiff = now_x - next_x;
         int yDiff = now_y - next_y;
@@ -87,7 +85,7 @@ public class RookPiece
             // 위로
             if(now_y > next_y){
                 for(int i = 1; i <= now_y - next_y; i++){
-                    if(location.LocationColor(now_x, next_y + i) == Color){
+                    if(location.LocationColor(now_x, next_y + i) == "Black"){
                         NextIsNull = false;
                     }
                 }
@@ -95,7 +93,7 @@ public class RookPiece
             // 아래로
             else if(now_y < next_y){
                 for(int i = 1; i <= next_y - now_y; i++){
-                    if(location.LocationColor(now_x, now_y + i) == Color){
+                    if(location.LocationColor(now_x, now_y + i) == "Black"){
                         NextIsNull = false;
                     }
                 }
@@ -113,7 +111,7 @@ public class RookPiece
             // 왼쪽으로
             if(now_x > next_x){
                 for(int i = 1; i <= now_x - next_x; i++){
-                    if(location.LocationColor(next_x + i, now_y) == Color){
+                    if(location.LocationColor(next_x + i, now_y) == "Black"){
                         NextIsNull = false;
                     }
                 }
@@ -121,7 +119,7 @@ public class RookPiece
             // 오른쪽으로
             else if(now_x < next_x){
                 for(int i = 1; i <= next_x - now_x; i++){
-                    if(location.LocationColor(now_x + i, now_y) == Color){
+                    if(location.LocationColor(now_x + i, now_y) == "Black"){
                         NextIsNull = false;
                     }
                 }
