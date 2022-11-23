5da309e6b4a75d81d240061fa8e53527c0464a82
Leejinwoo0405 <81578108+Leejinwoo0405@users.noreply.github.com>
2022-11-23 19:10:08 +0900

    Can Move
    I want to sex

--- src/decision_make/Decision_Making.java
+++ src/decision_make/Decision_Making.java
@@ -9,47 +9,78 @@ public class Decision_Making {
     public int[][] Chess_Current_Board; //  탐색이 끝난 말의 중요도가 저장되어 있는 부드
 
     public boolean CanMove(int now_dx, int now_dy, int next_dx, int next_dy, String[][] Board){
-        // Pawn Move
-        if(Board[now_dx][now_dy] == chess.BlackPawn){
-            if((now_dy - 1 == next_dy) && (next_dy >= 0 || next_dy <= 7)){
-                return true;
+
+        if((next_dx >= 0 || next_dx <= 7) && (next_dy >= 0 || next_dy <= 7)){
+            // Pawn Move
+            if(Board[now_dx][now_dy] == chess.BlackPawn){
+                if((now_dy - 1 == next_dy)){
+                    return true;
+                }
+                else if((now_dy - 2 == next_dy && now_dy == 1)) return true;
             }
-            else if((now_dy - 2 == next_dy && now_dy == 1) && (next_dy >= 0 || next_dy <= 7)) return true;
-        }
-        if(Board[now_dx][now_dy] == chess.WhitePawn){
-            if((now_dy - 1 == next_dy) && (next_dy >= 0 || next_dy <= 7)){
-                return true;
+            if(Board[now_dx][now_dy] == chess.WhitePawn){
+                if((now_dy - 1 == next_dy)){
+                    return true;
+                }
+                else if((now_dy - 2 == next_dy && now_dy == 6)) return true;
             }
-            else if((now_dy - 2 == next_dy && now_dy == 6) && (next_dy >= 0 || next_dy <= 7)) return true;
-        }
-
-        // Rook
-        if(Board[now_dx][now_dy] == chess.BlackRook || Board[now_dx][now_dy] == chess.WhiteRook){
-            for(int i=1;i<=7;i++){
-                if(now_dx + i == next_dx && now_dx + i <= 7) return true;
-                if(now_dx - i == next_dx && now_dx - i >= 0) return true;
-                if(now_dy + i == next_dy && now_dy + i <= 7) return true;
-                if(now_dy - i == next_dy && now_dy - i >= 0) return true;
+    
+            // Rook Move
+            if(Board[now_dx][now_dy] == chess.BlackRook || Board[now_dx][now_dy] == chess.WhiteRook){
+                for(int i=1;i<=7;i++){
+                    if(now_dx + i == next_dx && now_dx + i <= 7) return true;
+                    if(now_dx - i == next_dx && now_dx - i >= 0) return true;
+                    if(now_dy + i == next_dy && now_dy + i <= 7) return true;
+                    if(now_dy - i == next_dy && now_dy - i >= 0) return true;
+                }
             }
-        }
-        // Knight
-
-
-        // Bishop
-        if(Board[now_dx][now_dy] == chess.BlackBishop || Board[now_dx][now_dy] == chess.WhiteBishop){
-            for(int i=1;i<=7;i++){
-                if((now_dx + i == next_dx && now_dx + i <= 7) && (now_dy + i == next_dy && now_dy + i <= 7)) return true; // 아래로, 오른쪽 - 4 사분면
-                if((now_dx - i == next_dx && now_dx - i >= 0) && (now_dy + i == next_dy && now_dy + i <= 7)) return true; // 위로, 오른쪽 - 1 사분면
-                if((now_dx + i == next_dx && now_dx + i <= 7) && (now_dy - i == next_dy && now_dy - i >= 0)) return true; // 아래로, 왼쪽 - 3 사분면
-                if((now_dx - i == next_dx && now_dx - i >= 0) && (now_dy - i == next_dy && now_dy - i >= 0)) return true; // 위로, 왼쪽 - 2 사분면
+            // Knight Move
+            if(Board[now_dx][now_dy] == chess.BlackKnight || Board[now_dx][now_dy] == chess.WhiteKnight){
+                if(now_dx - 2 == next_dx && now_dy - 1 == next_dy) return true;
+                if(now_dx - 2 == next_dx && now_dy + 1 == next_dy) return true;
+                if(now_dx - 1 == next_dx && now_dy - 2 == next_dy) return true;
+                if(now_dx - 1 == next_dx && now_dy + 2 == next_dy) return true;
+                if(now_dx + 1 == next_dx && now_dy - 2 == next_dy) return true;
+                if(now_dx + 1 == next_dx && now_dy + 2 == next_dy) return true;
+                if(now_dx + 2 == next_dx && now_dy - 1 == next_dy) return true;
+                if(now_dx + 2 == next_dx && now_dy + 1 == next_dy) return true;
             }
+            // Bishop Move
+            if(Board[now_dx][now_dy] == chess.BlackBishop || Board[now_dx][now_dy] == chess.WhiteBishop){
+                for(int i=1;i<=7;i++){
+                    if((now_dx + i == next_dx && now_dx + i <= 7) && (now_dy + i == next_dy && now_dy + i <= 7)) return true; // 아래로, 오른쪽 - 4 사분면
+                    if((now_dx - i == next_dx && now_dx - i >= 0) && (now_dy + i == next_dy && now_dy + i <= 7)) return true; // 위로, 오른쪽 - 1 사분면
+                    if((now_dx + i == next_dx && now_dx + i <= 7) && (now_dy - i == next_dy && now_dy - i >= 0)) return true; // 아래로, 왼쪽 - 3 사분면
+                    if((now_dx - i == next_dx && now_dx - i >= 0) && (now_dy - i == next_dy && now_dy - i >= 0)) return true; // 위로, 왼쪽 - 2 사분면
+                }
+            }
+            // Queen
+            if(Board[now_dx][now_dy] == chess.BlackQueen || Board[now_dx][now_dy] == chess.WhiteQueen){
+                for(int i=1;i<=7;i++){
+                    if(now_dx + i == next_dx && now_dx + i <= 7) return true; // -y 방향
+                    if(now_dx - i == next_dx && now_dx - i >= 0) return true; // +y 방향
+                    if(now_dy + i == next_dy && now_dy + i <= 7) return true; // -x 방향
+                    if(now_dy - i == next_dy && now_dy - i >= 0) return true; // +x 방향
+                    if((now_dx + i == next_dx && now_dx + i <= 7) && (now_dy + i == next_dy && now_dy + i <= 7)) return true; // 아래로, 오른쪽 - 4 사분면
+                    if((now_dx - i == next_dx && now_dx - i >= 0) && (now_dy + i == next_dy && now_dy + i <= 7)) return true; // 위로, 오른쪽 - 1 사분면
+                    if((now_dx + i == next_dx && now_dx + i <= 7) && (now_dy - i == next_dy && now_dy - i >= 0)) return true; // 아래로, 왼쪽 - 3 사분면
+                    if((now_dx - i == next_dx && now_dx - i >= 0) && (now_dy - i == next_dy && now_dy - i >= 0)) return true; // 위로, 왼쪽 - 2 사분면
+                }
+            }
+            //King
+            if(Board[now_dx][now_dy] == chess.BlackKing && Board[now_dx][now_dy] == chess.WhiteKing){
+                if(now_dx + 1 == next_dx && now_dx + 1 <= 7) return true; // -y 방향
+                if(now_dx - 1 == next_dx && now_dx - 1 >= 0) return true; // +y 방향
+                if(now_dy + 1 == next_dy && now_dy + 1 <= 7) return true; // -x 방향
+                if(now_dy - 1 == next_dy && now_dy - 1 >= 0) return true; // +x 방향
+                if((now_dx + 1 == next_dx && now_dx + 1 <= 7) && (now_dy + 1 == next_dy && now_dy + 1 <= 7)) return true; // 아래로, 오른쪽 - 4 사분면
+                if((now_dx - 1 == next_dx && now_dx - 1 >= 0) && (now_dy + 1 == next_dy && now_dy + 1 <= 7)) return true; // 위로, 오른쪽 - 1 사분면
+                if((now_dx + 1 == next_dx && now_dx + 1 <= 7) && (now_dy - 1 == next_dy && now_dy - 1 >= 0)) return true; // 아래로, 왼쪽 - 3 사분면
+                if((now_dx - 1 == next_dx && now_dx - 1 >= 0) && (now_dy - 1 == next_dy && now_dy - 1 >= 0)) return true; // 위로, 왼쪽 - 2 사분면
+            }
+            return false;
         }
-        // Queen
-
-
-        //King
-
-        return false;
+        else return false;
     }
     //location.NowLocationPiece() // 현재위치
     //location.NextLocationPiece() // 나중위치
