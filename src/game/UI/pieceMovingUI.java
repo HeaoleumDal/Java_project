a493bdff8d7c2619b51a96a7e2f7ea9e992c676b
HeaoleumDal <88084889+HeaoleumDal@users.noreply.github.com>
2022-11-24 11:28:01 +0900

    Piece Moving UI Update Black
    블랙 말에 적용을 안 시킴

--- src/game/UI/pieceMovingUI.java
+++ src/game/UI/pieceMovingUI.java
@@ -116,6 +116,7 @@ public class pieceMovingUI {
                 }
                 else if(turn % 2 != 0)
                 {
+
                     System.out.println("BLACK TURN!\n");
                     BattleUI();
                     if(menu == 1)
@@ -126,14 +127,42 @@ public class pieceMovingUI {
                         StringTokenizer st_now = new StringTokenizer(br.readLine(), " ");
                         char black_now_x = st_now.nextToken().charAt(0);
                         char black_now_y = st_now.nextToken().charAt(0);
-                        System.out.println("말을 옮길 위치를 입력하세요");
-                        System.out.print("> ");
-                        StringTokenizer st_next = new StringTokenizer(br.readLine(), " ");
-                        char black_next_x = st_next.nextToken().charAt(0); 
-                        char black_next_y = st_next.nextToken().charAt(0);
-                        piecemoving.PieceMove(black_now_x, black_now_y, black_next_x, black_next_y, "Black");
-                        boardDrawing.DrawChessBoard();
-                        turn++;
+                        //지금 위치 입력
+                        if(CheckTokenizer(black_now_x, black_now_y) == true)
+                        {
+                            System.out.println("말을 옮길 위치를 입력하세요");
+                            System.out.print("> ");
+                            StringTokenizer st_next = new StringTokenizer(br.readLine(), " ");
+                            char black_next_x = st_next.nextToken().charAt(0); 
+                            char black_next_y = st_next.nextToken().charAt(0);
+                            //옮길 위치 입력
+                            if(CheckTokenizer(black_next_x, black_next_y) == true)
+                            {
+                                //옮기는 조건이 안되는 경우
+                                if(!piecemoving.PieceMove(black_now_x, black_now_y, black_next_x, black_next_y, "Black"))
+                                {
+                                    System.out.println("\n말을 옮길 수 없습니다. 다시 입력해주세요\n");
+                                    DrawMovingUI(1);
+                                }
+                                //옮기기 성공!
+                                else
+                                {
+                                    boardDrawing.DrawChessBoard();
+                                    turn++;  
+                                }
+                            }
+                            else if(CheckTokenizer(black_next_x, black_next_y) == false)
+                            {
+                                System.out.println("\n좌표 값이 없습니다. 다시 입력해주세요\n");
+                                DrawMovingUI(1);
+                            }
+                        }
+                        else if(CheckTokenizer(black_now_x, black_now_y) == false)
+                        {
+                            System.out.println("\n좌표 값이 없습니다. 다시 입력해주세요\n");
+                            DrawMovingUI(1);
+                        }
+
                     }
                     else if(menu == 2)
                     {
